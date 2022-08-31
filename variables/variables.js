1f102837cf83a4bdd57a79fb6f39426b8f7dcf35
theGeekkit <sonofgondolin16@gmail.com>
2022-08-31 15:59:26 -0500

    further variables with xyz

---
+++ variables/variables.js
@@ -0,0 +1,16 @@
+// var, let, const
+var x;
+let y;
+const z = 10;
+// z = 2; Cannot reassign values to constant variables
+
+console.log("x: ", x);
+console.log("y: ", y);
+console.log("z: ", z);
+
+x = 1;
+y = 2;
+
+console.log("x: ", x);
+console.log("y: ", y);
+console.log("z: ", z);
