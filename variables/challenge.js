00bd9ff6bd12439752671d1ab28d8c32dd191e30
theGeekkit <sonofgondolin16@gmail.com>
2022-09-07 19:46:32 -0500

    9-7-22

---
+++ variables/challenge.js
@@ -0,0 +1,9 @@
+
+function sum(a){
+    return function(b){
+        console.log(a + b)
+    }
+}
+
+sum(1)(2);
+sum(5)(-1);
---
+++ variables/objects-example.js
@@ -0,0 +1,13 @@
+let firstName = "John";
+let age = 35;
+let favoriteIceCream = 'Strawberry';
+
+let person = {
+    firstName: "John",
+    age: 35,
+    favoriteIceCream: 'Strawberry'
+}
+
+console.log(`My name is ${firstname}.`);
+console.log(`My age is ${age}`);
+console.log(`My favorite type pf ice cream is ${favoriteIceCream}`);
