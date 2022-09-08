ee3b180480943c7cd8bcf67a7c15f5876d4de525
theGeekkit <sonofgondolin16@gmail.com>
2022-09-07 19:58:03 -0500

    9-7-22

--- variables/objects-example.js
+++ variables/objects-example.js
@@ -5,9 +5,13 @@
 let person = {
     firstName: "John",
     age: 35,
-    favoriteIceCream: 'Strawberry'
+    favoriteIceCream: 'Strawberry',
+    vehicle: {
+        model: 'Hyundai'
+    }
 }
 
 console.log(`My name is ${person.firstName}.`);
 console.log(`My age is ${person.age}`);
 console.log(`My favorite type of ice cream is ${person["favoriteIceCream"]}`);
+console.log(`I own a ${person.vehicle.model}`);
